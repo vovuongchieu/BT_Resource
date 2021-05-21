7b835c674b3e95d53eb914a26d1eb4717cf9a126
cheiu <vovuongchieu@gmail.com>
2021-05-21 09:00:08 +0700

    Initial commit

---
+++ app/src/main/java/com/edu/csc/bt_resource/MainActivity.java
@@ -0,0 +1,15 @@
+package com.edu.csc.bt_resource;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
